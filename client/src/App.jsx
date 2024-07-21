b813ff4b42272cf89045c63c9ad8e78f10ce86fe
Truong Thai <52000714@student.tdtu.edu.vn>
2024-07-21 21:15:14 +0700

    merge App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,11 @@ import HomePage from "./pages/Home"
 import MainLayout from './MainLayout/MainLayout'
 import ProductManagement from "./pages/ProductManagement/ProductManagement";
 import Inventory from "./pages/Inventory/Inventory";
-<<<<<<< HEAD
 import Profile from "./pages/Profile/Profile";
 import LoginPage from "./pages/Login/Login";
 import RegesterPage from "./pages/Regester/Regester";
-=======
 import OrderManagement from "./pages/OrderManagement/OrderManagement";
 import CustomerManagement from "./pages/CustomerManagement/CustomerManagement";
->>>>>>> Nguyen-Ba-Thanh
 
 function App() {
 
@@ -24,12 +21,9 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route path="listOfProducts" element={<ProductManagement />} />
           <Route path="inventory" element={<Inventory />}/>
-<<<<<<< HEAD
           <Route path="profile" element={<Profile />}/>
-=======
           <Route path="listOfOrders" element={<OrderManagement />}/>
           <Route path="listOfCustomers" element={<CustomerManagement />}/>
->>>>>>> Nguyen-Ba-Thanh
         </Route>
       </Routes>
     </>
