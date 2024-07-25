9b257c36a0fb483098d904bc7bdf7ba6865b9817
Truong Thai <52000714@student.tdtu.edu.vn>
2024-07-25 14:18:25 +0700

    feat: get image

--- chorme_extension/src/background/background.ts
+++ chorme_extension/src/background/background.ts
@@ -1,40 +1,48 @@
-// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-//   console.log(msg);
-//   console.log(sender);
-//   sendResponse("Front the background Script");
+// // chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+// //   console.log(msg);
+// //   console.log(sender);
+// //   sendResponse("Front the background Script");
+// // });
+
+// const cacheName = "CHAT_API";
+
+// caches
+//   .open(cacheName)
+//   .then((cache) => {
+//     cache.keys().then((keys) => {
+//       keys.forEach((key) => {
+//         cache.match(key).then((response) => {
+//           if (response) {
+//             response.json().then((data) => {
+//               console.log("Data for", key.url, ":", data);
+
+//               // Send data to popup
+//               chrome.runtime.sendMessage({
+//                 action: "updatePopup",
+//                 data: { key: key.url, value: data },
+//               });
+//             });
+//           }
+//         });
+//       });
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error opening cache:", error);
+//   });
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "SEND_PARAGRAPH_TEXTS") {
+//     console.log("Received paragraph texts:", message.texts);
+//     // Xử lý dữ liệu theo ý muốn
+//     sendResponse({ status: "success" });
+//   }
 // });
-
-const cacheName = "CHAT_API";
-
-caches
-  .open(cacheName)
-  .then((cache) => {
-    cache.keys().then((keys) => {
-      keys.forEach((key) => {
-        cache.match(key).then((response) => {
-          if (response) {
-            response.json().then((data) => {
-              console.log("Data for", key.url, ":", data);
-
-              // Send data to popup
-              chrome.runtime.sendMessage({
-                action: "updatePopup",
-                data: { key: key.url, value: data },
-              });
-            });
-          }
-        });
-      });
-    });
-  })
-  .catch((error) => {
-    console.error("Error opening cache:", error);
-  });
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "SEND_PARAGRAPH_TEXTS") {
-    console.log("Received paragraph texts:", message.texts);
-    // Xử lý dữ liệu theo ý muốn
-    sendResponse({ status: "success" });
+  if (message.srcList) {
+    console.log("Image sources:", message.srcList);
+    chrome.storage.local.set({ srcList: message.srcList }, () => {
+      console.log("Image sources saved to storage");
+    });
   }
 });
--- chorme_extension/src/popup/components/shopee/ShopeeProduct.tsx
+++ chorme_extension/src/popup/components/shopee/ShopeeProduct.tsx
@@ -1,35 +1,147 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ShopeeProduct = () => {
-    return (
-        <form className="max-w-sm">
-            <div className='flex items-center'>
-                <label htmlFor="id_store" className="block w-[170px] align-center mb-2 text-xs font-medium text-gray-700 dark:text-white">Shop Name:</label>
-                <input type="text" id="id_store" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <div className='flex items-center'>
-                <label htmlFor="product_name" className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white">Code:</label>
-                <input type="text" id="product_name" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <div className='flex items-center'>
-                <label htmlFor="product_name" className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white">Cookies:</label>
-                <input type="text" id="product_name" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <div className='flex items-center'>
-                <label htmlFor="product_name" className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white">Phone Number:</label>
-                <input type="text" id="product_name" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <div className='flex items-center'>
-                <label htmlFor="product_name" className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white">Email:</label>
-                <input type="text" id="product_name" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <div className='flex items-center'>
-                <label htmlFor="product_name" className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white">Shop Id:</label>
-                <input type="text" id="product_name" className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500" />
-            </div>
-            <button type="submit" className="mt-3 text-white bg-gray-600 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center">Synchronized Product</button>
-        </form>
-    );
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    chrome.storage.local.get(["srcList"], (result) => {
+      if (result.srcList) {
+        setImages(result.srcList);
+      }
+    });
+  }, [images]);
+
+  const handleScrape = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0] && tabs[0].id) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { action: "scrapeImages" },
+          (response) => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+              // Handle the error, maybe the content script isn't ready
+              alert(
+                "There was an error communicating with the page. Please refresh the page and try again."
+              );
+            } else {
+              // Handle successful response
+              console.log("Images scraped successfully");
+            }
+          }
+        );
+      } else {
+        console.error("No active tab found");
+      }
+    });
+  };
+  return (
+    <>
+      <form className="max-w-sm">
+        <div className="flex items-center">
+          <label
+            htmlFor="id_store"
+            className="block w-[170px] align-center mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Shop Name:
+          </label>
+          <input
+            type="text"
+            id="id_store"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <label
+            htmlFor="product_name"
+            className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Code:
+          </label>
+          <input
+            type="text"
+            id="product_name"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <label
+            htmlFor="product_name"
+            className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Cookies:
+          </label>
+          <input
+            type="text"
+            id="product_name"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <label
+            htmlFor="product_name"
+            className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Phone Number:
+          </label>
+          <input
+            type="text"
+            id="product_name"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <label
+            htmlFor="product_name"
+            className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Email:
+          </label>
+          <input
+            type="text"
+            id="product_name"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <div className="flex items-center">
+          <label
+            htmlFor="product_name"
+            className="w-[170px] align-center block mb-2 text-xs font-medium text-gray-700 dark:text-white"
+          >
+            Shop Id:
+          </label>
+          <input
+            type="text"
+            id="product_name"
+            className="block w-full p-2 mb-2 text-xs text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500"
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-3 text-white bg-gray-600 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center"
+        >
+          Synchronized Product
+        </button>
+      </form>
+      <button
+        onClick={handleScrape}
+        className="mt-3 text-white bg-gray-600 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center"
+      >
+        Scrape Images
+      </button>
+      <ul>
+        {images.map((src, index) => (
+          <li key={index}>
+            <img
+              src={src}
+              alt={`Scraped image ${index}`}
+              style={{ width: "100px" }}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ShopeeProduct;
